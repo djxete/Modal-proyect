9224e464f059343791874825bf2b9e66292390b5
Ignacio Herráez <ignacio_hg_87@hotmail.com>
2020-12-09 15:24:40 +0100

    proyect done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-alert("probando");
+// ===== VARIABLES =============
+ 
+const btn = document.querySelector(".modal-btn");
+const btnClose = document.querySelector(".close-btn");
+const modalOverlay= document.querySelector(".modal-overlay");
+
+
+// ==== ADD EVENT LISTENER AND FUNCTIONS ======
+
+btn.addEventListener("click", ()=>{
+    modalOverlay.classList.add("open-modal");
+})
+
+btnClose.addEventListener("click", ()=>{
+    modalOverlay.classList.remove("open-modal");
+})
